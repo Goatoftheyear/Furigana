f145a97d0eef9642a79ba12d08f541a492045c70
px <lim.px100@gmail.com>
2026-04-04 00:29:43 +0800

    feat: add drawing of rectangle for detected text

--- app/src/main/java/com/example/furigana/ui/theme/composable/Greeting.kt
+++ app/src/main/java/com/example/furigana/ui/theme/composable/Greeting.kt
@@ -3,23 +3,50 @@ package com.example.furigana.ui.theme.composable
 import android.Manifest
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Point
+import android.graphics.Rect
+import android.view.OrientationEventListener
+import android.view.Surface
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.OptIn
 import androidx.camera.compose.CameraXViewfinder
+import androidx.camera.core.ExperimentalGetImage
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.ImageProxy
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+//import android.graphics.Path
+import androidx.compose.foundation.Canvas
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.toComposeRect
+import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.compose.LocalLifecycleOwner
@@ -29,6 +56,10 @@ import com.atilika.kuromoji.ipadic.Tokenizer
 import com.example.furigana.ui.theme.viewmodel.GreetingViewModel
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.japanese.JapaneseTextRecognizerOptions
+import kotlinx.coroutines.flow.MutableStateFlow
+import androidx.core.graphics.createBitmap
+import com.google.mlkit.vision.common.InputImage
+import java.util.concurrent.Executors
 
 @Composable
 fun Greeting(
@@ -41,29 +72,128 @@ fun Greeting(
     // need check if permission aren't granted
     // Camera Controller
     // ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
-    val surfaceRequest by viewModel.surfaceRequest.collectAsStateWithLifecycle()
     val context = LocalContext.current
+    val config = LocalConfiguration.current
+    val windowInfo = LocalWindowInfo.current
+    viewModel.imageAnalyzer.clearAnalyzer()
+
+    //TODO: put code below into viewModel
+//    var box: Array<out Point?>? = Array<Point>(4) { Point() }
+    val box = remember {mutableStateOf(Array<Point>(4) { Point() })}
+    val rect = remember {mutableStateOf(Rect().toComposeRect())}
+    // TODO: end of code
+    val surfaceRequest by viewModel.surfaceRequest.collectAsStateWithLifecycle()
+    var bitMapInMemory= remember { mutableStateOf(createBitmap(100, 100)) }
+    var taken = remember {mutableStateOf(false)}
     val cameraPermission = ContextCompat.checkSelfPermission(context, Manifest.permission.CAMERA)
     val permission = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.RequestPermission(),
         onResult = { res -> println(res) }
     )
     val tokenizer = Tokenizer()
-    val tokens = tokenizer.tokenize("お寿司が食べたい。")
-    for (token in tokens) {
-        println(token.getSurface() + "\t" + token.getAllFeatures())
-    }
-    var bitMapInMemory: Bitmap
+
+//    val tokens = tokenizer.tokenize("お寿司が食べたい。")
+//    for (token in tokens) {
+//        println(token.getSurface() + "\t" + token.getAllFeatures())
+//    }
+    val executor = remember { Executors.newSingleThreadExecutor() }
     val recognizer = TextRecognition.getClient(JapaneseTextRecognizerOptions.Builder().build())
+    var imageProxy:ImageProxy
+    val boxState = viewModel.box.collectAsState()
+    val pathState = viewModel.path.collectAsState()
+//    var path = Path().apply {
+//        box.value.mapIndexed { index, point ->
+//                if (index == 0) {
+//                    println("move ${point.x} ${point.y}")
+//                    moveTo( point.x.toFloat(),  point.y.toFloat())
+//                } else {
+//                    println("line ${point.x} ${point.y}")
+//                    lineTo(point.x.toFloat(), point.y.toFloat())
+//                }
+//        }
+//        close()
+//    }
+//    DisposableEffect(Unit) {
+//        val orientationEventListener =
+//            object : OrientationEventListener(context) {
+//                override fun onOrientationChanged(orientation: Int) {
+//                    if (orientation == ORIENTATION_UNKNOWN) return
+//
+//                    val rotation = when (orientation) {
+//                        in 45 until 135 -> Surface.ROTATION_270
+//                        in 135 until 225 -> Surface.ROTATION_180
+//                        in 225 until 315 -> Surface.ROTATION_90
+//                        else -> Surface.ROTATION_0
+//                    }
+//                    viewModel.imageCapture.targetRotation = rotation
+//                }
+//            }
+//            orientationEventListener.enable()
+//            onDispose { orientationEventListener.disable() }
+//        }
+
+//    DisposableEffect(Unit) {
+//        val listener = object : OrientationEventListener(context) {
+//            override fun onOrientationChanged(orientation: Int) {
+//                if (orientation == UNKNOWN_ORIENTATION) return
+//
+//                // Map degrees to Surface rotation constants
+//                val rotation = when (orientation) {
+//                    in 45 until 135 -> Surface.ROTATION_270
+//                    in 135 until 225 -> Surface.ROTATION_180
+//                    in 225 until 315 -> Surface.ROTATION_90
+//                    else -> Surface.ROTATION_0
+//                }
+//                // Update your ImageCapture use case
+//                imageCapture.targetRotation = rotation
+//            }
+//        }
+//        listener.enable()
+//        onDispose { listener.disable() }
+//    }
+
     LaunchedEffect(lifecycleOwner) {
         viewModel.bindToCamera(context.applicationContext, lifecycleOwner)
     }
-    if (cameraPermission == PackageManager.PERMISSION_GRANTED) {
+
+    println("width ${bitMapInMemory.value.width}")
+    if (taken.value) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Image(
+                modifier = Modifier
+                    .fillMaxSize(),
+                bitmap = bitMapInMemory.value.asImageBitmap(),
+                contentDescription = "hi"
+            )
+            Canvas(modifier = Modifier.fillMaxSize()) {
+                pathState.value.forEach {
+                    drawPath(
+                        path = it,
+                        color = Color.Black,
+                        style = Stroke(width = 10f)
+                    )
+                }
+            }
+        }
+    }
+    else if (cameraPermission == PackageManager.PERMISSION_GRANTED) {
         surfaceRequest?.let { request ->
             CameraXViewfinder(
                 surfaceRequest = request,
                 modifier = Modifier.fillMaxSize()
             )
+//            Canvas(modifier = Modifier.fillMaxSize()) {
+//                pathState.value.forEach {
+//                    drawPath(
+//                        path = it,
+//                        color = Color.Black,
+//                        style = Stroke(width = 10f)
+//                    )
+//                }
+//                println(rect.value)
+//                println(rect.value.topLeft)
+//                println(rect.value.size)
+//            }
             Column(
                 verticalArrangement = Arrangement.Center,
                 modifier = Modifier.fillMaxSize()
@@ -78,20 +208,116 @@ fun Greeting(
                                     println("error here")
                                 }
 
+//                                @OptIn(ExperimentalGetImage::class)
                                 override fun onCaptureSuccess(image: ImageProxy) {
+                                    println("test")
+                                    println("${image.imageInfo.rotationDegrees}")
+                                    taken.value = true
                                     super.onCaptureSuccess(image)
-                                    bitMapInMemory = image.toBitmap()
-                                    println("toire")
-                                    recognizer.process(bitMapInMemory, 0)
+                                    ContextCompat.getMainExecutor(context).execute {
+                                        bitMapInMemory.value = image.toBitmap()
+                                    }
+//                                    viewModel.imageAnalyzer.setAnalyzer(executor) { analysisImage ->
+//                                        image.image?.let {
+//                                                image ->
+//                                            println("test image rotation ${analysisImage.imageInfo.rotationDegrees}")
+//                                            val img = InputImage.fromMediaImage(
+//                                                image,
+//                                                analysisImage.imageInfo.rotationDegrees
+//                                            )
+//                                            recognizer.process(img).addOnSuccessListener {
+//                                                text ->
+//                                                pathState.value = Path().apply {
+//                                                    box.value.mapIndexed { index, point ->
+//                                                        if (index == 0) {
+////                                                            println("move ${point.x} ${point.y}")
+//                                                            moveTo( point.x.toFloat()/2,  point.y.toFloat()/2)
+//                                                        } else {
+//                                                            lineTo(point.x.toFloat()/2, point.y.toFloat()/2)
+//                                                        }
+//                                                    }
+//                                                    close()
+//                                                }
+//                                                val ans = text.textBlocks
+//                                                for (line in ans) {
+//                                                    println("test cornerPoints ${line.cornerPoints}")
+//                                                    println("test boundingBox${line.boundingBox}")
+//
+//                                                    println("test top ${line.boundingBox?.top}")
+//                                                    println("test bottom ${line.boundingBox?.bottom}")
+//                                                    println("test left ${line.boundingBox?.left}")
+//                                                    println("test right ${line.boundingBox?.right}")
+//                                                }
+//                                                analysisImage.close()
+//                                            }
+//                                        }
+//                                    }
+//                                    bitMapInMemory.value = image.toBitmap()
+//                                    recognizer.process(bitMapInMemory.value, 0)
+                                    recognizer.process(InputImage.fromBitmap(image.toBitmap(), 0))
                                         .addOnSuccessListener { text ->
+
                                             val ans = text.textBlocks
                                             for (line in ans) {
+//                                                line.cornerPoints?.mapIndexed { index, it ->
+//                                                    println("$index $it")
+//
+//                                                }
+                                                box.value = line.cornerPoints!!
+
+                                                rect.value = line.boundingBox!!.toComposeRect()
+//                                                val scaleHorizontal = config.screenWidthDp
+//                                                val scaleHorizontal = config.screenHeightDp
+                                                val scaleHorizontal = windowInfo.containerSize.width.toFloat() / image.toBitmap().width
+                                                val scaleVertical = windowInfo.containerSize.height.toFloat() / image.toBitmap().height
+
+//                                                println("testtest ${line.cornerPoints}")
+//                                                println("testtest ${line.boundingBox}")
+//
+//                                                println("testtop ${line.boundingBox?.top}")
+//                                                println("testbottom ${line.boundingBox?.bottom}")
+//                                                println("testleft ${line.boundingBox?.left}")
+//                                                println("testright ${line.boundingBox?.right}")
+
+//                                                val path = Path().apply {
+//                                                    box.value.mapIndexed { index, point ->
+//                                                        if (index == 0) {
+////                                                            println("move ${point.x} ${point.y}")
+//                                                            moveTo( point.x.toFloat()/2,  point.y.toFloat()/2)
+//                                                        } else {
+////                                                            println("line ${point.x} ${point.y}")
+//                                                            lineTo(point.x.toFloat()/2, point.y.toFloat()/2)
+//                                                        }
+//                                                    }
+//                                                    close()
+//                                                }
+                                                println("test $scaleHorizontal")
+                                                println("test $scaleVertical")
+                                                val path = Path().apply {
+                                                    line.cornerPoints!!.mapIndexed { index, point ->
+                                                        if (index == 0) {
+                                                            println("move ${point.x * scaleHorizontal} ${point.y * scaleVertical}")
+                                                            moveTo( point.x.toFloat() * scaleHorizontal,  point.y.toFloat() * scaleVertical)
+                                                        } else {
+                                                            println("move ${point.x * scaleHorizontal} ${point.y * scaleVertical}")
+                                                            lineTo(point.x.toFloat() * scaleHorizontal, point.y.toFloat()  * scaleVertical)
+                                                        }
+                                                    }
+                                                    close()
+                                                }
+                                                viewModel.setPath(path)
+
+//                                                val mutableBitMap = image.toBitmap().copy(Bitmap.Config.ARGB_8888, true)
+//                                                val canvas = Canvas(mutableBitMap)
+//                                                canvas.drawPath(path, Paint().apply {
+//                                                    color = android.graphics.Color.RED
+//                                                })
                                                 val lineText = line.text
                                                 println(lineText)
                                                 val tokens = tokenizer.tokenize(lineText)
-                                                for (token in tokens) {
-                                                    println(token.getSurface() + "\t" + token.getAllFeatures())
-                                                }
+//                                                for (token in tokens) {
+//                                                    println(token.getSurface() + "\t" + token.getAllFeatures())
+//                                                }
                                             }
                                         }
                                 }
@@ -103,7 +329,9 @@ fun Greeting(
                         text = "Hmmage",
                         textAlign = TextAlign.Center
                     )
+
                 }
+
             }
         }
     } else {
--- app/src/main/java/com/example/furigana/ui/theme/viewmodel/GreetingViewModel.kt
+++ app/src/main/java/com/example/furigana/ui/theme/viewmodel/GreetingViewModel.kt
@@ -1,17 +1,20 @@
 package com.example.furigana.ui.theme.viewmodel
 
+import android.app.Application
 import android.content.Context
-import android.graphics.Bitmap
-import android.view.Surface
+import android.graphics.Point
+import androidx.camera.core.AspectRatio.RATIO_16_9
 import androidx.camera.core.CameraSelector.DEFAULT_BACK_CAMERA
+import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageCapture
 import androidx.camera.core.Preview
+import androidx.camera.core.SurfaceOrientedMeteringPointFactory
 import androidx.camera.core.SurfaceRequest
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.lifecycle.awaitInstance
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.graphics.Path
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.ViewModel
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.japanese.JapaneseTextRecognizerOptions
 import kotlinx.coroutines.awaitCancellation
@@ -19,29 +22,96 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import java.util.concurrent.Executors
 
-class GreetingViewModel : ViewModel() {
+class GreetingViewModel(application: Application) : AndroidViewModel(application) {
+
+//    private val context = getApplication<Application>().applicationContext
+
+    //    val orientationEventListener =
+//        object : OrientationEventListener(context) {
+//            override fun onOrientationChanged(orientation: Int) {
+//                if (orientation == ORIENTATION_UNKNOWN) return
+//
+//                val rotation = when (orientation) {
+//                    in 45 until 135 -> Surface.ROTATION_270
+//                    in 135 until 225 -> Surface.ROTATION_180
+//                    in 225 until 315 -> Surface.ROTATION_90
+//                    else -> Surface.ROTATION_0
+//                }
+//                imageCapture.targetRotation = rotation
+//            }
+//        }
+    private val _box = MutableStateFlow(Array<Point>(4) { Point() })
+    val box = _box.asStateFlow()
+
+    private var _path = MutableStateFlow( mutableListOf(Path().apply {
+        _box.value.mapIndexed { index, point ->
+            if (index == 0) {
+                println("move ${point.x} ${point.y}")
+                moveTo(point.x.toFloat(), point.y.toFloat())
+            } else {
+                println("line ${point.x} ${point.y}")
+                lineTo(point.x.toFloat(), point.y.toFloat())
+            }
+        }
+        close()
+    }))
+
+    val path = _path.asStateFlow()
+
     private val _surfaceRequest = MutableStateFlow<SurfaceRequest?>(null)
+    private var surfaceMeteringPointFactory: SurfaceOrientedMeteringPointFactory? = null
+
     val surfaceRequest: StateFlow<SurfaceRequest?> = _surfaceRequest.asStateFlow()
 
     val recognizer = TextRecognition.getClient(JapaneseTextRecognizerOptions.Builder().build())
 
+    fun setPath(path: Path) {
+        _path.value.add(path)
+        _path.value = _path.value.toMutableList()
+    }
     private val cameraPreviewUseCase = Preview.Builder().build().apply {
         setSurfaceProvider { newSurfaceRequest ->
-            _surfaceRequest.update { newSurfaceRequest
+            _surfaceRequest.update {
+                newSurfaceRequest
+
             }
+            surfaceMeteringPointFactory = SurfaceOrientedMeteringPointFactory(
+                newSurfaceRequest.resolution.width.toFloat(),
+                newSurfaceRequest.resolution.height.toFloat()
+            )
         }
     }
 
+
     val imageCapture: ImageCapture = ImageCapture.Builder()
-        .setTargetRotation(Surface.ROTATION_0)
+//        .setResolutionSelector(
+//            ResolutionSelector.Builder()
+//                .setResolutionStrategy(                ResolutionStrategy(
+//                    Size(1980, 1080),
+//                    ResolutionStrategy.FALLBACK_RULE_CLOSEST_HIGHER_THEN_LOWER))
+//                .build()
+//        )
+        //TODO: fix deprecation
+        .setTargetAspectRatio(RATIO_16_9)
+        .build()
+    val cameraExecutor = Executors.newSingleThreadExecutor()
+
+    val imageAnalyzer = ImageAnalysis.Builder()
+        .setTargetAspectRatio(RATIO_16_9)
+        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
         .build()
 
     suspend fun bindToCamera(appContext: Context, lifecycleOwner: LifecycleOwner) {
         val processCameraProvider = ProcessCameraProvider.awaitInstance(appContext)
         processCameraProvider.bindToLifecycle(
-            lifecycleOwner, DEFAULT_BACK_CAMERA,imageCapture, cameraPreviewUseCase
+            lifecycleOwner, DEFAULT_BACK_CAMERA, imageCapture, imageAnalyzer, cameraPreviewUseCase
         )
-        try { awaitCancellation() } finally {processCameraProvider.unbindAll()}
+        try {
+            awaitCancellation()
+        } finally {
+            processCameraProvider.unbindAll()
+        }
     }
 }
