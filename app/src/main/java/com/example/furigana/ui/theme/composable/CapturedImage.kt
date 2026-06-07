c2d5ca71bafd7092ed9817cc27cd436d88b56a14
px <lim.px100@gmail.com>
2026-06-07 23:48:22 +0800

    feat: add boxes paths when finished processing

--- app/src/main/java/com/example/furigana/ui/theme/composable/CapturedImage.kt
+++ app/src/main/java/com/example/furigana/ui/theme/composable/CapturedImage.kt
@@ -1,5 +1,6 @@
 package com.example.furigana.ui.theme.composable
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
@@ -13,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.furigana.ui.theme.viewmodel.GreetingViewModel
@@ -24,6 +26,7 @@ fun CapturedImage(
     //TODO: add processing
     val bitmap = viewModel.imageBitmap.collectAsState()
     val isProcessing = viewModel.isProcessing.collectAsState()
+    val path = viewModel.path.collectAsState()
     Box(modifier = Modifier.fillMaxSize()) {
         Image(
             modifier = Modifier
@@ -42,6 +45,15 @@ fun CapturedImage(
             CircularProgressIndicator(color = Color.White)
         }
     } else {
-
+        Canvas(modifier = Modifier.fillMaxSize()) {
+            for (p in path.value) {
+                drawPath(
+                    path = p,
+                    color = Color.Black,
+                    style = Stroke(width = 10f)
+                )
+            }
+        }
     }
+
 }
