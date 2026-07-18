75e32192e13a627574c3bf6f402019bf3f13ae6a
px <lim.px100@gmail.com>
2026-07-18 15:52:03 +0800

    refactor: put creating path boxes to another method

--- app/src/main/java/com/example/furigana/ui/theme/viewmodel/ImageToResultViewModel.kt
+++ app/src/main/java/com/example/furigana/ui/theme/viewmodel/ImageToResultViewModel.kt
@@ -22,6 +22,7 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.viewModelScope
 import com.atilika.kuromoji.ipadic.Tokenizer
 import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.Text
 import com.google.mlkit.vision.text.TextRecognition
 import com.google.mlkit.vision.text.japanese.JapaneseTextRecognizerOptions
 import kotlinx.coroutines.Dispatchers
@@ -33,6 +34,7 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlin.collections.mutableListOf
 import kotlin.math.abs
 
 class ImageToResultViewModel(application: Application) : AndroidViewModel(application) {
@@ -151,67 +153,7 @@ class ImageToResultViewModel(application: Application) : AndroidViewModel(applic
                     ) continue
                     coroutineScope {
                         async(Dispatchers.Default) {
-                            _box.value = line.cornerPoints!!
-                            //TODO: kept it for experimentation
-//                    rect.value = line.boundingBox!!.toComposeRect()
-
-                            val scaleHorizontal = screenWidth / _imageBitmap.value.width
-                            val scaleVertical = screenHeight / _imageBitmap.value.height
-                            var prev = Point(
-                                line.cornerPoints?.get(0)?.x ?: 0,
-                                line.cornerPoints?.get(0)?.y ?: 0
-                            )
-                            val findLongestSide = mutableMapOf<String, Float>()
-                            findLongestSide.put("x", 0.0f)
-                            findLongestSide.put("y", 0.0f)
-                            line.cornerPoints!!.map {
-                                if (abs(it.x - prev.x) > findLongestSide.get("x")!!) {
-                                    findLongestSide.put(
-                                        "x",
-                                        abs(it.x - prev.x).toFloat()
-                                    )
-                                }
-                                if (abs(it.y - prev.y) > findLongestSide.get("y")!!) {
-                                    findLongestSide.put(
-                                        "y",
-                                        abs(it.y - prev.y).toFloat()
-                                    )
-                                }
-                                prev = it
-                            }
-                            val startTextPointReference = mutableMapOf<String, Point>()
-                            if (findLongestSide["x"]!! > findLongestSide["y"]!!) {
-                                startTextPointReference.put(
-                                    "horizontal",
-                                    line.cornerPoints!![0]
-                                )
-                            } else {
-                                startTextPointReference.put(
-                                    "horizontal",
-                                    line.cornerPoints!![1]
-                                )
-
-                            }
-                            val path = Path().apply {
-                                line.cornerPoints!!.mapIndexed { index, point ->
-
-                                    if (index == 0) {
-                                        println("move ${point.x * scaleHorizontal} ${point.y * scaleVertical}")
-                                        moveTo(
-                                            point.x.toFloat() * scaleHorizontal,
-                                            point.y.toFloat() * scaleVertical
-                                        )
-                                    } else {
-                                        println("move ${point.x * scaleHorizontal} ${point.y * scaleVertical}")
-                                        lineTo(
-                                            point.x.toFloat() * scaleHorizontal,
-                                            point.y.toFloat() * scaleVertical
-                                        )
-                                    }
-                                }
-                                close()
-                            }
-                            paths.add(path)
+                            createBoxOnText(line,paths,screenWidth,screenHeight)
                         }
                         async(Dispatchers.Default) {
                             tokenizeLine(lineText,allTokenResults)
@@ -223,6 +165,70 @@ class ImageToResultViewModel(application: Application) : AndroidViewModel(applic
                 }
             }
             .addOnFailureListener { setIsProcessing(false) }
+    }
+    fun createBoxOnText(line: Text.TextBlock, paths:MutableList<Path>,
+                        screenWidth: Float, screenHeight: Float) {
+        _box.value = line.cornerPoints!!
+        //TODO: kept it for experimentation
+//                    rect.value = line.boundingBox!!.toComposeRect()
+
+        val scaleHorizontal = screenWidth / _imageBitmap.value.width
+        val scaleVertical = screenHeight / _imageBitmap.value.height
+        var prev = Point(
+            line.cornerPoints?.get(0)?.x ?: 0,
+            line.cornerPoints?.get(0)?.y ?: 0
+        )
+        val findLongestSide = mutableMapOf<String, Float>()
+        findLongestSide.put("x", 0.0f)
+        findLongestSide.put("y", 0.0f)
+        line.cornerPoints!!.map {
+            if (abs(it.x - prev.x) > findLongestSide.get("x")!!) {
+                findLongestSide.put(
+                    "x",
+                    abs(it.x - prev.x).toFloat()
+                )
+            }
+            if (abs(it.y - prev.y) > findLongestSide.get("y")!!) {
+                findLongestSide.put(
+                    "y",
+                    abs(it.y - prev.y).toFloat()
+                )
+            }
+            prev = it
+        }
+        val startTextPointReference = mutableMapOf<String, Point>()
+        if (findLongestSide["x"]!! > findLongestSide["y"]!!) {
+            startTextPointReference.put(
+                "horizontal",
+                line.cornerPoints!![0]
+            )
+        } else {
+            startTextPointReference.put(
+                "horizontal",
+                line.cornerPoints!![1]
+            )
+
+        }
+        val path = Path().apply {
+            line.cornerPoints!!.mapIndexed { index, point ->
+
+                if (index == 0) {
+                    println("move ${point.x * scaleHorizontal} ${point.y * scaleVertical}")
+                    moveTo(
+                        point.x.toFloat() * scaleHorizontal,
+                        point.y.toFloat() * scaleVertical
+                    )
+                } else {
+                    println("move ${point.x * scaleHorizontal} ${point.y * scaleVertical}")
+                    lineTo(
+                        point.x.toFloat() * scaleHorizontal,
+                        point.y.toFloat() * scaleVertical
+                    )
+                }
+            }
+            close()
+        }
+        paths.add(path)
     }
     fun tokenizeLine(lineText: String, allTokenResults: MutableList<MutableMap<String, String>>) {
         val furiganaOutput: MutableMap<String, String> = mutableMapOf()
